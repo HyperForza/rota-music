b25dcce745ba2057c0298b1158cda0c7f8218f12
HyperForza <67697041+HyperForza@users.noreply.github.com>
2021-06-23 06:54:40 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,7 @@ var prefix = ayarlar.prefix;
 
 module.exports = client => {
 var oyun = [
-        "Salvo Code | Müzik Botu",
+        "Rota Music | r!help",
     ];
 
     setInterval(function() {
@@ -20,6 +20,5 @@ var oyun = [
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setActivity(`Kanala Abone Ol`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
 };
